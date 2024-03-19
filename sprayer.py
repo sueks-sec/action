e182c0f6adafccdd9086ebb363f7a0e4d92311f4
sueks-sec <92654395+sueks-sec@users.noreply.github.com>
2024-03-19 14:12:53 -0400

    Update sprayer.py

--- sprayer.py
+++ sprayer.py
@@ -32,7 +32,7 @@ def send_login_request():
     post_headers = {
         "Accept": "application/json",
         "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36.",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.50",
     }
 
     try:
